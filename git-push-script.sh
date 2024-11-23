a975881b03f591682143f57cb9834872e2c4a3a7
Lalit-N97 <lalitnegi1997@gmail.com>
2024-11-23 17:11:27 +0530

    $msg

--- git-push-script.sh
+++ git-push-script.sh
@@ -3,10 +3,12 @@ filename=$(ls -t1 | head -1)
 msg=$(head -n 2 $filename | tail -1)
 echo $filename
 echo $msg
+echo "..........Before Staging.........."
 git add $filename
-echo "..........Before Committing.........."
 git status
-$(git commit -m \"$msg\")
+echo "..........After Staging.........."
+git status
+git commit -m '$msg'
 echo "..........After Committing.........."
 git status
 # git push

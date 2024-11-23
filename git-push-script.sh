1525e1478362fbb909ece45dcfdd23c136a537e7
Lalit-N97 <lalitnegi1997@gmail.com>
2024-11-23 17:23:53 +0530

    automate git add, commit and push

--- git-push-script.sh
+++ git-push-script.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 filename=$(ls -t1 | head -1)
 msg=$(head -n 2 $filename | tail -1)
-echo $filename
-echo $msg
-echo "..........Before Staging.........."
+# echo $filename
+# echo $msg
+echo "..........Before Staging............"
+git status
 git add $filename
-git status
-echo "..........After Staging.........."
-git status
-git commit -m '$msg'
+git commit -m " $msg " # added spaces around the variable message to avoid bash saying passing multiple 
+# parameters to the commit command when a space separated commmit message is used
 echo "..........After Committing.........."
 git status
-# git push
+git push
